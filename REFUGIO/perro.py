5a6af9dc2232735605f527e110edef5a4c7a68ba
joafernandez <efernandez@fie.undef.edu.ar>
2025-08-26 11:50:10 -0300

    agrego perrito

---
+++ REFUGIO/perro.py
@@ -0,0 +1,13 @@
+
+from mascota import Mascota 
+
+class Perro(Mascota):
+    def __init__(self, nombre, tReahb, edad, fIngreso, doc):
+        super() .__init__(nombre, tReahb, edad, fIngreso)
+        self.doc=doc
+        
+        #joa
+        
+       
+    
+        
